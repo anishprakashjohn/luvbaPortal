75b34ec5c6173692142c1c16bb9a0f081fd11dee
anish <anishprakashjohn@gmail.com>
2017-11-07 21:49:27 +1100

    FirstCode

---
+++ index.php
@@ -0,0 +1,183 @@
+<?php
+
+    session_start();
+
+    $error = "";    
+
+    if (array_key_exists("logout", $_GET)) {
+        
+        unset($_SESSION);
+        setcookie("id", "", time() - 60*60);
+        $_COOKIE["id"] = "";  
+        
+    } else if ((array_key_exists("id", $_SESSION) AND $_SESSION['id']) OR (array_key_exists("id", $_COOKIE) AND $_COOKIE['id'])) {
+        
+        header("Location: loggedinpage.php");
+        
+    }
+
+    if (array_key_exists("submit", $_POST)) {
+        
+        $link = mysqli_connect("localhost", "cl29-secretdi", "D-fnT^Hbz", "cl29-secretdi");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+        
+        
+        
+        if (!$_POST['email']) {
+            
+            $error .= "An email address is required<br>";
+            
+        } 
+        
+        if (!$_POST['password']) {
+            
+            $error .= "A password is required<br>";
+            
+        } 
+        
+        if ($error != "") {
+            
+            $error = "<p>There were error(s) in your form:</p>".$error;
+            
+        } else {
+            
+            if ($_POST['signUp'] == '1') {
+            
+                $query = "SELECT id FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."' LIMIT 1";
+
+                $result = mysqli_query($link, $query);
+
+                if (mysqli_num_rows($result) > 0) {
+
+                    $error = "That email address is taken.";
+
+                } else {
+
+                    $query = "INSERT INTO `users` (`email`, `password`) VALUES ('".mysqli_real_escape_string($link, $_POST['email'])."', '".mysqli_real_escape_string($link, $_POST['password'])."')";
+
+                    if (!mysqli_query($link, $query)) {
+
+                        $error = "<p>Could not sign you up - please try again later.</p>";
+
+                    } else {
+
+                        $query = "UPDATE `users` SET password = '".md5(md5(mysqli_insert_id($link)).$_POST['password'])."' WHERE id = ".mysqli_insert_id($link)." LIMIT 1";
+
+                        mysqli_query($link, $query);
+
+                        $_SESSION['id'] = mysqli_insert_id($link);
+
+                        if ($_POST['stayLoggedIn'] == '1') {
+
+                            setcookie("id", mysqli_insert_id($link), time() + 60*60*24*365);
+
+                        } 
+
+                        header("Location: loggedinpage.php");
+
+                    }
+
+                } 
+                
+            } else {
+                    
+                    $query = "SELECT * FROM `users` WHERE email = '".mysqli_real_escape_string($link, $_POST['email'])."'";
+                
+                    $result = mysqli_query($link, $query);
+                
+                    $row = mysqli_fetch_array($result);
+                
+                    if (isset($row)) {
+                        
+                        $hashedPassword = md5(md5($row['id']).$_POST['password']);
+                        
+                        if ($hashedPassword == $row['password']) {
+                            
+                            $_SESSION['id'] = $row['id'];
+                            
+                            if ($_POST['stayLoggedIn'] == '1') {
+
+                                setcookie("id", $row['id'], time() + 60*60*24*365);
+
+                            } 
+
+                            header("Location: loggedinpage.php");
+                                
+                        } else {
+                            
+                            $error = "That email/password combination could not be found.";
+                            
+                        }
+                        
+                    } else {
+                        
+                        $error = "That email/password combination could not be found.";
+                        
+                    }
+                    
+                }
+            
+        }
+        
+        
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+    <h1>LUV BA Portal</h1>
+    <p> Welcome to the LUV BA Portal</p>
+    <div id="error"><?php echo $error; ?></div>
+
+    <form method="post">
+
+        <input type="email" name="email" placeholder="Your Email">
+    
+        <input type="password" name="password" placeholder="Password">
+    
+        <input type="checkbox" name="stayLoggedIn" value=1>
+    
+        <input type="hidden" name="signUp" value="1">
+        
+        <input type="submit" name="submit" value="Sign Up!">
+
+    </form>
+
+<form method="post">
+
+    <input type="email" name="email" placeholder="Your Email">
+    
+    <input type="password" name="password" placeholder="Password">
+    
+    <input type="checkbox" name="stayLoggedIn" value=1>
+    
+    <input type="hidden" name="signUp" value="0">
+        
+    <input type="submit" name="submit" value="Log In!">
+
+</form>
+</div>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
+
